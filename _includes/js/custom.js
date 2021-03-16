b1223f88b7eb64068eb3b988e7446acb55640211
Kai Schwarz <kschwarz@hexonet.net>
2021-03-16 14:19:26 +0100

    fix(style): re-applied and .prettierignore fixed

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,16 +1,16 @@
-window.addEventListener('load', function () {
-  function strikethrough(){
-      document.body.innerHTML = document.body.innerHTML.replace(
-        /\~\~(.+?)\~\~/gim,
-        '<del>$1</del>'
-      );
+window.addEventListener("load", function () {
+  function strikethrough() {
+    document.body.innerHTML = document.body.innerHTML.replace(
+      /\~\~(.+?)\~\~/gim,
+      "<del>$1</del>"
+    )
   }
-  strikethrough();
+  strikethrough()
 
-  $(window).on('activate.bs.scrollspy', function () {
-    const $eL = $('#site-nav .nav li a.active').get(0);
-    if ($eL){
-      $eL.scrollIntoView();
+  $(window).on("activate.bs.scrollspy", function () {
+    const $eL = $("#site-nav .nav li a.active").get(0)
+    if ($eL) {
+      $eL.scrollIntoView()
     }
   })
-});
+})
