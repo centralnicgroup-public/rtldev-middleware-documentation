fa2a9b5507b96ca884745821b6bbeb6f2a00c811
Kai Schwarz <kschwarz@hexonet.net>
2023-01-19 11:13:15 +0000

    ci(docker): added post-start.sh script

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# install npm global deps
+npm ci
+
+# update rubygems and deps
+gem update --system
+gem install bundler jekyll
+
+# Install the version of Bundler.
+if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
+    cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
+fi
+
+# If there's a Gemfile, then run `bundle install`
+# It's assumed that the Gemfile will install Jekyll too
+if [ -f Gemfile ]; then
+    bundle install
+fi
+
+bundle exec jekyll serve -H 0.0.0.0 -t
